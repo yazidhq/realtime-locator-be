29b76950646d5326be57b3ce5169bcc5624c9884
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2025-10-15 11:27:13 +0700

    feat: livetrack repo injection and validate

--- internal/delivery/event/websocket/handler.go
+++ internal/delivery/event/websocket/handler.go
@@ -1,18 +1,73 @@
 package websocket
 
 import (
+	"TeamTrackerBE/internal/domain/repository"
 	"TeamTrackerBE/internal/utils/responses"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
 )
 
-func LiveTrack(c *gin.Context) {
+type LiveTrackHandler struct {
+	repoGroup repository.GroupRepository
+	repoUser repository.UserRepository
+	repoGroupParticipant repository.GroupParticipantRepository
+}
+
+func NewLiveTrackHandler(
+	rg *repository.GroupRepository,
+	ru *repository.UserRepository,
+	rgp *repository.GroupParticipantRepository,
+	) *LiveTrackHandler {
+	return &LiveTrackHandler{
+		repoGroup: *rg,
+		repoUser: *ru,
+		repoGroupParticipant: *rgp,
+	}
+}
+
+func (u *LiveTrackHandler) LiveTrack(c *gin.Context) {
 	groupID := c.Query("group_id")
 	userID := c.Query("user_id")
 
 	if groupID == "" || userID == "" {
-		responses.NewBadRequestError("group id and user id is required")
+		responses.Error(c, http.StatusBadRequest, "group id and user id is required")
+		return
 	}
 
-	ServeWs(c.Writer, c.Request, groupID, userID)
+	groupIDParse, errGroupID := uuid.Parse(groupID);
+	if errGroupID != nil {
+		responses.Error(c, http.StatusBadRequest, "invalid group id, must be uuid type")
+		return
+	}
+
+	if _, err := u.repoGroup.FindById(groupIDParse); err != nil {
+		responses.Error(c, http.StatusBadRequest, "group id not found in group")
+		return
+	}
+	
+	userIDParse, errUserID := uuid.Parse(userID);
+	if errUserID != nil {
+		responses.Error(c, http.StatusBadRequest, "invalid user id, must be uuid type")
+		return
+	}
+
+	if _, err := u.repoUser.FindById(userIDParse); err != nil {
+		responses.Error(c, http.StatusBadRequest, "user id not found in user")
+		return
+	}
+
+	if _, err := u.repoGroupParticipant.FindByGroupIDUserID(groupIDParse, userIDParse); err != nil {
+		responses.Error(c, http.StatusBadRequest, "user is not group participant")
+		return
+	}
+
+	if websocket.IsWebSocketUpgrade(c.Request) {
+		ServeWs(c.Writer, c.Request, groupID, userID)
+		return
+	}
+
+	responses.Success(c, "All good mate, NEXT CONNECT!", nil)
 }
--- internal/delivery/event/websocket/router.go
+++ internal/delivery/event/websocket/router.go
@@ -3,8 +3,17 @@ package websocket
 import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+
+	"TeamTrackerBE/internal/delivery/http/middleware"
+	_repo "TeamTrackerBE/internal/domain/repository"
 )
 
 func InitWSRoutes(r *gin.Engine, db *gorm.DB) {
-	r.GET("/api/live_track", LiveTrack)
+	groupRepo := _repo.NewGroupRepository(db)
+	userRepo := _repo.NewUserRepository(db)
+	userGroupParticipant := _repo.NewGroupParticipantRepository(db)
+	liveTrackHandler := NewLiveTrackHandler(groupRepo, userRepo, userGroupParticipant)
+
+	r.Use(middleware.AuthMiddleware())
+	r.GET("/api/live_track", liveTrackHandler.LiveTrack)
 }
