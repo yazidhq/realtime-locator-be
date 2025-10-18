e71742204252a868e7747186d59c186ee53c7912
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2025-10-18 10:38:36 +0700

    update: dto message

--- internal/delivery/event/websocket/dto.go
+++ internal/delivery/event/websocket/dto.go
@@ -1,19 +1,26 @@
 package websocket
 
 type LocationMessage struct {
+	Type      string  `json:"type"`
 	UserID    string  `json:"user_id"`
 	Latitude  float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
-	Timestamp string  `json:"timestamp"`
+}
+
+type ChatMessage struct {
+	Type    string `json:"type"`
+	UserID  string `json:"user_id"`
+	Message string `json:"message"`
+}
+
+type CrossedRadiusMessage struct {
+	Type    string `json:"type"`
+	UserID  string `json:"user_id"`
+	Message string `json:"message"`
 }
 
 type RadiusArea struct {
 	Radius    float64 `json:"radius"`
 	CenterLat float64 `json:"center_lat"`
 	CenterLon float64 `json:"center_lon"`
-}
-
-type CrossedRadiusMessage struct {
-	Type    string `json:"type"`
-	Message string `json:"message"`
 }
