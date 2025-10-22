5d5bde0c3bf1b658c228a160728bafaa74ca45c3
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2025-10-22 11:37:13 +0700

    delete: contact status

--- internal/domain/model/index.go
+++ internal/domain/model/index.go
@@ -8,29 +8,17 @@ import (
 
 type (
 	Role           string
-	ContactStatus  string
 )
 
 const (
 	Superadmin Role = "superadmin"
 	Admin      Role = "admin"
 	Member     Role = "member"
-
-	Pending  ContactStatus = "pending"
-	Accepted ContactStatus = "accepted"
 )
 
 func (r Role) IsValid() bool {
 	switch r {
 	case Superadmin, Admin, Member:
-		return true
-	}
-	return false
-}
-
-func (r ContactStatus) IsValid() bool {
-	switch r {
-	case Pending, Accepted:
 		return true
 	}
 	return false
@@ -62,26 +50,9 @@ func (r *Role) Scan(value any) error {
 	return nil
 }
 
-func (r *ContactStatus) Scan(value any) error {
-	if err := scanString(r, value); err != nil {
-		return err
-	}
-	if !r.IsValid() {
-		return responses.NewBadRequestError(fmt.Sprintf("invalid Contact Status value: %s", *r))
-	}
-	return nil
-}
-
 func (r Role) Value() (driver.Value, error) {
 	if !r.IsValid() {
 		return nil, responses.NewBadRequestError(fmt.Sprintf("invalid Role value: %s", r))
-	}
-	return string(r), nil
-}
-
-func (r ContactStatus) Value() (driver.Value, error) {
-	if !r.IsValid() {
-		return nil, responses.NewBadRequestError(fmt.Sprintf("invalid Contact Status value: %s", r))
 	}
 	return string(r), nil
 }
