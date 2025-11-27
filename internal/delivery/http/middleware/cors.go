b720e336e76cee8ee83e5582128eccfc3fa78e00
yazid dhiaulhaq ismail <yaziddhiaulhaqismail@yahoo.co.id>
2025-11-27 15:59:27 +0700

    feat: update CORS middleware to allow all origins

--- internal/delivery/http/middleware/cors.go
+++ internal/delivery/http/middleware/cors.go
@@ -9,7 +9,7 @@ import (
 
 func CorsMiddleware() gin.HandlerFunc {
     return cors.New(cors.Config{
-        AllowOrigins: []string{"http://localhost:3000", "http://127.0.0.1:3000"},
+        AllowOrigins: []string{"http://localhost:3000", "http://127.0.0.1:3000", "*"},
         AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
         AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
         ExposeHeaders: []string{"Content-Length", "Authorization"},
